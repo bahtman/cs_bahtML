1a32b5e9c2a377c0f5a918326afe05c7f18342e8
Anton Baht <anton@baht.dk>
2023-07-21 03:27:51 +0200

    point in polygon

--- cs_bahtml/filtering.py
+++ cs_bahtml/filtering.py
@@ -1,17 +1,13 @@
 import geopandas as gpd
 import pandas as pd
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point
 
 
-def points_in_polygon(df: pd.DataFrame, polygon: gpd.GeoDataFrame, join: str = "inner"):
-    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x, df.y))
-    gdf = gdf.sjoin(polygon,how = join, predicate = "within")
-    df_out = pd.DataFrame(gdf.drop(columns='geometry'))
-    return df_out
+def point_in_polygon(point:tuple, polygon: Polygon):
+    point = Point(point)
+    b_contain = polygon.contains(point)
+    return b_contain
 
 def build_polygon(points):
     polygon = Polygon(points)
-    gdf = gpd.GeoDataFrame()
-    gdf['geometry'] = None
-    gdf.loc[0,'geometry'] = polygon
-    return gdf
+    return polygon
